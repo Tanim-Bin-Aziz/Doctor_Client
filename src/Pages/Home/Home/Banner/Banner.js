fc739692637aa395b90d9b922324c5f646eafe00
Tanim-Bin-Aziz <ahmedxaziz@gmail.com>
2023-09-09 16:30:42 +0600

    new feature added

--- src/Pages/Home/Home/Banner/Banner.js
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -4,9 +4,9 @@ const Banner = () => {
     return (
         <div className="hero">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} className="s rounded-lg w-1/2 shadow-2xl" alt='' />
+                <img src={chair} className="rounded-lg lg:w-1/2 shadow-2xl" alt='' />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <h1 className="text-5xl font-bold">Welcome to Next Dent!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
