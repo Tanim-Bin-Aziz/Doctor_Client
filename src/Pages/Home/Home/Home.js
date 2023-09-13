e4fd2d485387c0f25956987df38064d74ebe335b
Tanim-Bin-Aziz <ahmedxaziz@gmail.com>
2023-09-14 02:30:19 +0600

    review added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,6 +4,7 @@ import InfoCards from './InfoCards/InfoCards';
 import Services from './Services/Services';
 import Test from './Test/Test';
 import MakeAppointment from './MakeAppointment/MakeAppointment';
+import Testimonial from './Testimonial/Testimonial';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
             <Services></Services>
             <Test></Test>
             <MakeAppointment></MakeAppointment>
+            <Testimonial></Testimonial>
         </div>
     );
 };
