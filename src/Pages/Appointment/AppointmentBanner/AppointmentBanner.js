b2e14540a535d4686815adf3637edc576d07313e
Tanim-Bin-Aziz <ahmedxaziz@gmail.com>
2023-09-16 20:31:49 +0600

    new feature date picker added

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chair from '../../../assets/images//chair.png';
-
+import { DayPicker } from 'react-day-picker';
 const AppointmentBanner = () => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
     return (
         <header>
             <div className="hero">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={chair} alt="dentist chair" className="max-w-sm rounded-lg shadow-2xl" />
-
+                    <DayPicker
+                        mode='single'
+                        selected={selectedDate}
+                        onSelect={setSelectedDate} />
                 </div>
             </div>
         </header>
